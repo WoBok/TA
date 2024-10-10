a1b3eb02d8c9dee1e9755f4b77289c7e2bfc4d0b
WoBok <darwin.vinci.wkc@gmail.com>
2024-10-10 22:36:44 +0800

    Lesson05

--- Maya/Week_03/Lesson_05/Lesson_05.py
+++ Maya/Week_03/Lesson_05/Lesson_05.py
@@ -1,41 +1,44 @@
+import pymel.core as pm
 import maya.cmds as cmds
 import math
 
-def generateCube(index,scaleValue,posValue):
-    cmds.polyCube(n=f"cube{index}")
-    cmds.setAttr(f"cube{index}.scale", scaleValue[0], scaleValue[1], scaleValue[2], type="float3")
-    cmds.setAttr(f"cube{index}.rotateY", -6*i)
-    cmds.move(posValue[0], posValue[1], posValue[2])
+def generateCube(scaleValue,posValue):
+    cube = pm.polyCube()[0]
+    cube.scale.set(scaleValue)
+    cube.rotateY.set(-6*i)
+    cube.translate.set(posValue)
 
 x,y,z = 0,0,0
-bbox = cmds.exactWorldBoundingBox("Panel")
-origin = cmds.xform("Panel",q=1,t=1,ws=1)
-radius = abs(bbox[3] - bbox[0]) / 2 - 0.5
+clock_face = pm.PyNode("Dial|Panel")
+bbox = clock_face.getBoundingBox()
+origin = clock_face.translate.get()
+radius = bbox.w / 2 - 0.5
 angle = 2 * math.pi / 60
 for i in range(60):
     currentAngle = angle * i
-    x = radius * math.cos(currentAngle) + origin[0]
+    x = radius * math.cos(currentAngle) + origin.x
     y = origin[1]
-    z = radius * math.sin(currentAngle) + origin[2]
+    z = radius * math.sin(currentAngle) + origin.z
     if i%5 == 0:
-        generateCube(i,(0.7,0.2,0.2),(x,y,z))
+        generateCube((0.7,0.2,0.2),(x,y,z))
     else:
-        generateCube(i,(0.5,0.1,0.1),(x,y,z))
+        generateCube((0.5,0.1,0.1),(x,y,z))
 
 def combineRotation(firstNode,secondNode,b,operation):
-    floatMath = cmds.shadingNode("floatMath", au=1)
-    cmds.connectAttr(f"{firstNode}.rotateY", f"{floatMath}.floatA", f=1)
-    cmds.connectAttr(f"{floatMath}.outFloat", f"{secondNode}.rotateY", f=1)
-    cmds.setAttr(f"{floatMath}.floatB", b)
-    cmds.setAttr(f"{floatMath}.operation", operation)
+    floatMath = pm.createNode("floatMath")
+    firstNode.rotateY >> floatMath.floatA
+    floatMath.outFloat >> secondNode.rotateY
+    floatMath.floatB.set(b)
+    floatMath.operation.set(operation)
 
-combineRotation("Second", "Minute" ,60 ,3)
-combineRotation("Minute", "Hour" ,12 ,3)
+combineRotation(pm.PyNode("Dial|Second"), pm.PyNode("Dial|Minute") ,60 ,3)
+combineRotation(pm.PyNode("Dial|Minute"), pm.PyNode("Dial|Hour") ,12 ,3)
 
 attributes = {"Seconds":59,"Minutes":59,"Hours":11}
 for key,value in attributes.items():
-    if not cmds.objExists(f"Panel.{key}"):
-        cmds.addAttr("|Dial|Panel", ln=key, at="double", min=0, max=value, dv=0, k=1)
+    if not clock_face.hasAttr(key):
+        clock_face.addAttr(ln=key, at='double', min=0, max=value, dv=0, k=True);
+        #cmds.addAttr("|Dial|Panel", ln=key, at="double", min=0, max=value, dv=0, k=1)
 
 floatMaths = []
 for i in range(5):
