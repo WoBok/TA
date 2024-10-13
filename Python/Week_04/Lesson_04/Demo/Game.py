2c436a032646510229f61d829156fa830a068219
WoBok <darwin.vinci.wkc@gmail.com>
2024-10-13 12:08:21 +0800

    Python_Week_04_Lesson_04

---
+++ Python/Week_04/Lesson_04/Demo/Game.py
@@ -0,0 +1,6 @@
+print('This is module.')
+
+def func():
+    print('Game func')
+if __name__ == '__main__':
+    print('These do not need to be imported.')
---
+++ Python/Week_04/Lesson_04/lesson04.py
@@ -0,0 +1,10 @@
+import sys
+from pprint import pprint
+import Demo
+from Demo import Game as G
+
+#pprint(sys.path)
+#pprint(sys.version)
+print(type(Demo))
+print(type(G))
+G.func()
