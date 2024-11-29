5c612c0926c5fa997b85821d4ca509810d8cc622
WoBok <darwin.vinci.wkc@gmail.com>
2024-11-29 09:40:15 +0800

    Homework_08

--- Homework/Homework_08/homework_08.py
+++ Homework/Homework_08/homework_08.py
@@ -6,15 +6,15 @@ class TimeTable:
     def __init__(self):
         pass
 
+    # 创建时间表文件夹
     def _create_dir(self, path):
         try:
             if not os.path.exists(path):
                 os.makedirs(path)
-            else:
-                print(f"Directory already exists: {path}")
         except Exception as e:
             print(f"Error creating directories: {e}")
 
+    # 创建时间表文件
     def _create_file(self, file_path):
         try:
             with open(file_path, "w") as file:
@@ -22,6 +22,7 @@ class TimeTable:
         except Exception as e:
             print(f"Error creating file: {e}")
 
+    # 创建时间表
     def _create_time_table(self, path, file_name):
         directory_path = os.path.dirname(path)
         file_path = os.path.join(directory_path, file_name)
@@ -29,6 +30,7 @@ class TimeTable:
         self._create_dir(directory_path)
         self._create_file(file_path)
 
+    # 遍历解析json得到的字典
     def _iterate_time_table(self, date_dict, path):
         for key, value in date_dict.items():
             if isinstance(value, list):
@@ -38,6 +40,7 @@ class TimeTable:
                 path = f"{path}/{key}"
                 self._create_time_table(path, value.get("file", "unknown.csv"))
 
+    # 解析json文件
     def _parse_json(self, root_directory):
         with open("date_and_time_table.json", "r") as json_file:
             data = json.load(json_file)
